d1ca7a6f100ae5dfe8847324d316b3646b765350
Rafael Miranda <rafaelpadovezi@gmail.com>
2023-06-21 22:39:40 +0100

    test: create game test

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pytruco
+
+## Details
+
+### Entities
+
+![Entities](docs/pytruco-entities.png "Entities")
--- tests/apps/core/api/v1/test_views.py
+++ tests/apps/core/api/v1/test_views.py
@@ -1,6 +1,6 @@
 from django.test import Client, TestCase
 
-from pytruco.apps.core.models import Game
+from pytruco.apps.core.models import Game, Hand, PlayerCard
 
 
 class GameViewTest(TestCase):
@@ -20,3 +20,25 @@ class GameViewTest(TestCase):
                 "player2": db_game.player2.id,
             },
         )
+        self.assertEqual(db_game.round_set.count(), 1)
+        db_round = db_game.round_set.all()[0]
+        self.assertEqual(db_round.is_current, True)
+        self.assertEqual(db_round.who_starts, 1)
+        self.assertEqual(db_round.number, 1)
+        self.assertEqual(db_round.points_in_game, 1)
+        self.assertEqual(db_round.points_being_raised, 1)
+
+        self.assertEqual(db_round.hand_set.count(), 1)
+        db_hand = db_round.hand_set.all()[0]
+        self.assertEqual(db_hand.is_current, True)
+        self.assertEqual(db_hand.number, 1)
+        self.assertEqual(db_hand.result, Hand.HandResult.Playing)
+
+        player1_cards = PlayerCard.objects.filter(
+            round__id=db_round.id, player__id=db_game.player1.id
+        )
+        self.assertEqual(player1_cards.count(), 3)
+        player2_cards = PlayerCard.objects.filter(
+            round__id=db_round.id, player__id=db_game.player2.id
+        )
+        self.assertEqual(player2_cards.count(), 3)
